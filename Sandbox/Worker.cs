e6cd49b8467cd7ee9e311658454d91d495668826
anne55x9 <anne55x9@edu.easj.dk>
2016-11-08 13:14:13 +0100

    test1

---
+++ Sandbox/Worker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class Worker : Employee 
+    {
+        public virtual string WorkerSkill
+        {
+            get { return WorkerSkill; }
+            set { WorkerSkill = value; }
+        }
+
+        public Worker(String name, int salaryPerMonth, string Workerskill)
+            : base (name, salaryPerMonth)
+        {
+
+        }
+
+        //bla bla bla 
+         
+
+    }
+}
